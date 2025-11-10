651ba272f8c0309eacf267db1f77de0017712d2f
gerso <Gerson130>
2025-11-10 11:51:59 -0300

    Se agrega vista de calendario de riego + métods get, post y delete

---
+++ Controllers/CalendarioController.cs
@@ -0,0 +1,66 @@
+﻿using Invernadero.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Invernadero.Controllers
+{
+    public class CalendarioController : Controller
+    {
+        public readonly InvernaderoContext _context;
+
+        public CalendarioController(InvernaderoContext context) { 
+            _context = context;
+        }
+
+        public async Task <IActionResult> Create()
+        { 
+            return View(await _context.CalendarioDeRiego.ToListAsync());
+        }
+
+        /*public IActionResult Create()
+        { 
+            return View(new CalendarioDeRiego());
+        }*/
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Fecha, Tipo, NivelAgua")] CalendarioDeRiego calendario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.CalendarioDeRiego.Add(calendario);
+                await _context.SaveChangesAsync();  
+                return RedirectToAction(nameof(Create));
+            }
+            return View(calendario);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var especialidad = await _context.CalendarioDeRiego.FindAsync(id);
+
+            if (especialidad == null)
+                return NotFound();
+
+            return View(especialidad);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        { 
+            if (id == null)
+                return NotFound();
+
+            var calendario = await _context.CalendarioDeRiego.FindAsync(id);
+            Console.WriteLine(id);
+
+            if (calendario == null)
+                return NotFound();
+
+            _context.CalendarioDeRiego.Remove(calendario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Create));
+        }
+    }
+}
--- Models/CalendarioDeRiego.cs
+++ Models/CalendarioDeRiego.cs
@@ -1,10 +1,13 @@
-﻿namespace Invernadero.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invernadero.Models
 {
     public class CalendarioDeRiego
     {
         public int Id { get; set; }
-        public DateTime Fecha { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
         public string Tipo { get; set; }
+        [Display(Name ="Nivel de agua (ml)")]
         public int NivelAgua { get; set; } 
         public List<Notifica> Notificacion {  get; set; } = new List<Notifica>();
         public List<CalendarioDeRiegoUsuario> CalendarioRiegoUsuario { get; set; } = new List<CalendarioDeRiegoUsuario>();
