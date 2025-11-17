55d0416bcfe47a37292503e2f119211b39bed2ad
gerso <Gerson130>
2025-11-17 18:41:49 -0300

    se agrega inicio de sesión

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using Invernadero.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Invernadero.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public readonly InvernaderoContext _context;
+
+        public UsuarioController(InvernaderoContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string nombre, string contrasenia)
+        {
+            if (contrasenia == null)
+            {
+                return NotFound();
+            }
+            var usuario = await _context.Usuario.FirstOrDefaultAsync(m => m.Contrasenia == contrasenia);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("Create", "Calendario");
+        }
+    }
+}
