e66a982df0f65228a0d11cdf96e84a189949737d
Nelson <nelson.h.labrin8@gmail.com>
2025-11-18 15:07:31 -0300

    Modifique HomeController para que Home pueda acceder a calendario para madar la alerta, y hice que mandara las alertas en el menu pricipal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,43 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Invernadero.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
 
 namespace Invernadero.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly InvernaderoContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, InvernaderoContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var now = DateTime.Now;
+
+        var riegosPendientes = await _context.CalendarioDeRiego
+            .Where(c => c.Fecha.Date == now.Date && c.Fecha > now)
+            .OrderBy(c => c.Fecha)
+            .ToListAsync();
+
+        var notificaciones = riegosPendientes
+            .Select(r => new NotificacionViewModel
+            {
+                Mensaje = $"Riego pendiente, Tipo: {r.Tipo} ({r.NivelAgua} ml)"
+            })
+            .ToList();
+
+        ViewBag.NotificacionesRiego = notificaciones;
+
         return View();
     }
 
@@ -28,4 +51,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+public class NotificacionViewModel
+{
+    public string Mensaje { get; set; }
 }
